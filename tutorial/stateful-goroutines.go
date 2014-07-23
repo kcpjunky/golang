1da87931c42ddc39daae18fa43e747cdb594a16e
kcpjunky <kcpjunky@gmail.com>
2014-07-24 05:03:14 +0900

    stateful-goroutines

---
+++ tutorial/stateful-goroutines.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+  "sync/atomic"
+  "time"
+)
+
+type read0p struct {
+  key int
+  resp chan int
+}
+
+type write0p struct {
+  key int
+  val int
+  resp chan bool
+}
+
+func main() {
+  
+  var ops int64 = 0
+
+  reads := make(chan *read0p)
+  writes := make(chan *write0p)
+
+  go func() {
+    var state = make(map[int]int)
+
+    for {
+      select {
+        case read := <-reads:
+          read.resp <- state[read.key]
+        case write := <-writes:
+          state[write.key] = write.val
+          write.resp <- true
+      }
+    }
+  }()
+
+  for r := 0; r < 100; r++ {
+    go func() {
+      for {
+        read := &read0p {
+          key: rand.Intn(5),
+          resp: make(chan int)}
+        reads <- read
+        <-read.resp
+        atomic.AddInt64(&ops, 1)
+      }
+    }()
+  }
+
+  time.Sleep(time.Second)
+
+  opsFinal := atomic.LoadInt64(&ops)
+  fmt.Println("ops:", opsFinal)
+}
+
